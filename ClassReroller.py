a34fc55a2a73fa64ed4d563e236c4b0a4972a27c
lachlanmonty <montylachlan@gmail.com>
2022-07-30 18:42:50 +1000

    formating

--- ClassReroller.py
+++ ClassReroller.py
@@ -1,13 +1,13 @@
 from calendar import c
 import tkinter as tk
-from tkinter import * 
+from tkinter import *
 import webbrowser
 import random
 
-#Main Gui Body
+# Main Gui Body
 m = tk.Tk()
-#Title of Program
-m.title('Class Reroller')
+# Title of Program
+m.title("Class Reroller")
 i = BooleanVar()
 j = BooleanVar()
 k = BooleanVar()
@@ -19,34 +19,34 @@ Healer = ["Disc Priest", "Holy Priest", "Resto Druid", "Resto Sham", "Holy Pala"
 mLabel = Label(m)
 mLabel.pack
 
-#Set Window Size
+# Set Window Size
 m.geometry("500x500")
 m.resizable(False, False)
 
-#Functions
+# Functions
 def Classselect():
     global mLabel
     if i.get() == True & j.get() == True & k.get() == True & l.get() == True:
-        mLabel.config(text="You Cannot Have this Combination") 
+        mLabel.config(text="You Cannot Have this Combination")
     elif i.get() == True & j.get() == True & k.get() == True:
         mLabel.config(text="Welcome to Life of Kirbi")
     elif i.get() == True & j.get() == True:
         a = random.choice(Tanks)
-        mLabel.config(text="Tank Carries harder, play: "  + a)    
+        mLabel.config(text="Tank Carries harder, play: " + a)
     elif i.get() == True & l.get() == True:
-        webbrowser.open("https://www.youtube.com/watch?v=q225u-qGpas")   
+        webbrowser.open("https://www.youtube.com/watch?v=q225u-qGpas")
     elif j.get() == True & l.get() == True:
         mLabel.config(text="Go watch Star Wars, trying to break the program logic?")
     elif i.get() == True & k.get() == True:
         mLabel.config(text="Play DPS")
     elif j.get() == True & k.get() == True:
-        mLabel.config(text="Play DPS")    
+        mLabel.config(text="Play DPS")
     elif k.get() == True & l.get() == True:
         mLabel.config(text="Stop it Nangs")
     elif i.get() == True:
         a = random.choice(Tanks)
         mLabel.config(text=a)
-    elif j.get() ==True:
+    elif j.get() == True:
         b = random.choice(Healer)
         mLabel.config(text=b)
     elif k.get() == True:
@@ -54,20 +54,21 @@ def Classselect():
     elif l.get() == True:
         webbrowser.open("https://www.youtube.com/watch?v=1VYD9zbXzpo")
 
-#Widgets
-c1 = Checkbutton(m, text = 'Tank',variable=i)
+
+# Widgets
+c1 = Checkbutton(m, text="Tank", variable=i)
 c1.pack()
-c2 = Checkbutton(m, text = 'Healer',variable=j)
+c2 = Checkbutton(m, text="Healer", variable=j)
 c2.pack()
-c3 = Checkbutton(m, text = 'DPS',variable=k)
+c3 = Checkbutton(m, text="DPS", variable=k)
 c3.pack()
-c4 = Checkbutton(m, text = 'Mage',variable=l)
+c4 = Checkbutton(m, text="Mage", variable=l)
 c4.pack()
 
-b1 = Button(m, text = "What Class Shall I Play?",height=3, width=50, command=Classselect)
+b1 = Button(m, text="What Class Shall I Play?", height=3, width=50, command=Classselect)
 b1.pack()
 
 mLabel.pack()
 
-#Just need this kappa
+# Just need this kappa
 m.mainloop()
